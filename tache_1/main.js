ab2032e720c02fe0a37cb5adac1e9eda55a36625
OUMAR THIOUNE <omarthioune996@gmail.com>
2023-02-20 10:11:45 +0200

    mon commentaire sur mon travail

---
+++ tache_1/main.js
@@ -0,0 +1,28 @@
+new CircleProgress(" .progress-un", {
+
+	max: 100,
+	value:97,
+	textFormat: "percent",
+
+});
+new CircleProgress(" .progress-deux", {
+   
+	max: 100,
+	value: 78,
+	textFormat: "percent",
+
+});
+new CircleProgress(" .progress-trois", {
+
+	max: 100,
+	value: 67,
+	textFormat: "percent",
+
+});
+new CircleProgress(" .progress-quatre", {
+
+	max: 100,
+	value: 97,
+	textFormat: "percent",
+
+});
